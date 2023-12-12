64cc9746f83b23213b3dace3c0201a7da4cd7f5d
JACQUELINE <jacquelinesmith190@gmail.com>
2023-12-12 13:42:23 -0800

    Add model

--- team_management/models.py
+++ team_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class TeamMember(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    ROLE_CHOICES = [
+        ('REG', 'Regular'),
+        ('ADM', 'Admin'),
+    ]
+    role = models.CharField(max_length=3, choices=ROLE_CHOICES, default='REG')
