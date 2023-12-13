80a9284e0ab8fd1609ece7fc2aed2420bcd9b276
JACQUELINE <jacquelinesmith190@gmail.com>
2023-12-13 00:55:52 -0800

    Add team member functionality

--- team_management/urls.py
+++ team_management/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import home_view, TeamMemberListView
+from .views import home_view, TeamMemberListView, TeamMemberCreateView
 
 urlpatterns = [
     path('', home_view, name='home'),  
     path('team/', TeamMemberListView.as_view(), name='team_member_list'),
+    path('team/add/', TeamMemberCreateView.as_view(), name='team_member_add'),
 ]
--- team_management/views.py
+++ team_management/views.py
@@ -1,12 +1,18 @@
 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from django.shortcuts import render
 from .models import TeamMember
+from .forms import TeamMemberForm 
 from django.urls import reverse_lazy
 
 class TeamMemberListView(ListView):
     model = TeamMember
     template_name = 'team_management/team_member_list.html'
+
+class TeamMemberCreateView(CreateView):
+    form_class = TeamMemberForm
+    template_name = 'team_management/team_member_form.html'
+    success_url = reverse_lazy('team_member_list')  
 
 def home_view(request):
     context = {}
